1e68054d38dd15c5e55d4cf36026e451fb9417cb
someda <someda@isenshi.com>
2014-05-15 01:16:59 +0900

    basic implementation done

--- lib/fluent/plugin/out_mackerel.rb
+++ lib/fluent/plugin/out_mackerel.rb
@@ -2,10 +2,20 @@ module Fluent
   class MackerelOutput < Fluent::BufferedOutput
     Fluent::Plugin.register_output('mackerel', self)
 
-    # config_param :hoge, :string, :default => 'hoge'
-
     config_param :api_key, :string
+    config_param :hostid, :string, :default => nil
+    config_param :hostid_path, :string, :default => nil
     config_param :metrics_prefix, :string
+    config_param :out_keys, :string
+
+    config_param :flush_interval, :time, :default => 1
+
+    attr_reader :mackerel
+
+    # Define `log` method for v0.10.42 or earlier
+    unless method_defined?(:log)
+      define_method("log") { $log }
+    end
 
     def initialize
       super
@@ -13,6 +23,18 @@ module Fluent
 
     def configure(conf)
       super
+
+      @mackerel = Mackerel.new(conf['api_key'])
+      @out_keys = @out_keys.split(',')
+
+      if @hostid.nil? and @hostid_path.nil?
+        raise Fluent::ConfigError, "Either 'hostid' or 'hostid_path' must be specifed."
+      end
+
+      unless @hostid_path.nil?
+        @hostid = File.open(@hostid_path).read
+      end
+
     end
 
     def start
@@ -28,10 +50,60 @@ module Fluent
     end
 
     def write(chunk)
-      records = []
+      metrics = []
       chunk.msgpack_each do |(tag,time,record)|
+        out_keys.map do |key|
+          metrics << {
+            'hostId' => @hostid,
+            'value' => record[key].to_f,
+            'time' => time,
+            'name' => "%s.%s" % [@metrics_prefix, key]
+          }
+        end
+      end
+
+      begin
+        @mackerel.post_metrics(metrics)
+      rescue => e
+        log.error("out_mackerel:", :error_class => e.class, :error => e.message)
       end
     end
 
   end
+
+  class Mackerel
+
+    USER_AGENT = "fluent-plugin-mackerel Ruby/#{RUBY_VERSION}"
+
+    def initialize(api_key)
+      require 'net/http'
+      require 'json'
+
+      @api_key = api_key
+      @http = Net::HTTP.new('mackerel.io', 443)
+      @http.use_ssl = true
+    end
+
+    def post_metrics(metrics)
+      req = Net::HTTP::Post.new('/api/v0/tsdb', initheader = {
+        'X-Api-Key' => @api_key,
+        'Content-Type' =>'application/json',
+        'User-Agent' => USER_AGENT
+      })
+      req.body = metrics.to_json
+      res = @http.request(req)
+
+      if res.is_a?(Net::HTTPUnauthorized)
+        raise MackerelError, "invalid api key used. check api_key in your configuration."
+      end
+
+      unless res and res.is_a?(Net::HTTPSuccess)
+        raise MackerelError, "failed to post, code: #{res.code}"
+      end
+    end
+
+  end
+
+  class MackerelError < RuntimeError; end
+
 end
--- test/plugin/test_out_mackerel.rb
+++ test/plugin/test_out_mackerel.rb
@@ -7,6 +7,18 @@ class MackerelOutputTest < Test::Unit::TestCase
   end
 
   CONFIG = %[
+    type mackerel
+    api_key 123456
+    hostid xyz
+    metrics_prefix service
+    out_keys val1,val2,val3
+  ]
+
+  CONFIG_NOHOST = %[
+    type mackerel
+    api_key 123456
+    metrics_prefix service
+    out_keys val1,val2,val3
   ]
 
   def create_driver(conf = CONFIG, tag='test')
@@ -14,6 +26,38 @@ class MackerelOutputTest < Test::Unit::TestCase
   end
 
   def test_configure
+
+    assert_raise(Fluent::ConfigError) {
+      d = create_driver('')
+    }
+
+    assert_raise(Fluent::ConfigError) {
+      d = create_driver(CONFIG_NOHOST)
+    }
+
+    d = create_driver()
+    assert_equal d.instance.instance_variable_get(:@api_key), '123456'
+    assert_equal d.instance.instance_variable_get(:@hostid), 'xyz'
+    assert_equal d.instance.instance_variable_get(:@metrics_prefix), 'service'
+    assert_equal d.instance.instance_variable_get(:@out_keys), ['val1','val2','val3']
+  end
+
+  def test_write
+    d = create_driver()
+    stub(d.instance.mackerel).post_metrics([
+      {"hostId"=>"xyz", "value"=>1.0, "time"=>1399997498, "name"=>"service.val1"},
+      {"hostId"=>"xyz", "value"=>2.0, "time"=>1399997498, "name"=>"service.val2"},
+      {"hostId"=>"xyz", "value"=>3.0, "time"=>1399997498, "name"=>"service.val3"},
+      {"hostId"=>"xyz", "value"=>5.0, "time"=>1399997498, "name"=>"service.val1"},
+      {"hostId"=>"xyz", "value"=>6.0, "time"=>1399997498, "name"=>"service.val2"},
+      {"hostId"=>"xyz", "value"=>7.0, "time"=>1399997498, "name"=>"service.val3"},
+    ])
+
+    ENV["TZ"]="Asia/Tokyo"
+    t = Time.strptime('2014-05-14 01:11:38', '%Y-%m-%d %T')
+    d.emit({'val1' => 1, 'val2' => 2, 'val3' => 3, 'val4' => 4}, t)
+    d.emit({'val1' => 5, 'val2' => 6, 'val3' => 7, 'val4' => 8}, t)
+    d.run()
   end
 
 
