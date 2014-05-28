011b14625f33ef013875e9c6e8791176ef3811f3
hatz48 <hatz48@hatena.ne.jp>
2014-05-28 19:52:31 +0900

    remove/add prefix option

--- lib/fluent/plugin/out_mackerel_hostid_tag.rb
+++ lib/fluent/plugin/out_mackerel_hostid_tag.rb
@@ -6,6 +6,8 @@ module Fluent
     config_param :hostid_path, :string, :default => '/var/lib/mackerel-agent/id'
     config_param :add_to, :string
     config_param :key_name, :default => nil
+    config_param :add_prefix, :string
+    config_param :remove_prefix, :string
 
     # Define `log` method for v0.10.42 or earlier
     unless method_defined?(:log)
@@ -22,13 +24,25 @@ module Fluent
       if @add_to == 'record' and @key_name.nil?
         raise Fluent::ConfigError, "'key_name' must be specified"
       end
+      if @remove_prefix
+        @removed_prefix_string = @remove_prefix + '.'
+        @removed_length = @removed_prefix_string.length
+      end
+      @added_prefix_string = @add_prefix + '.' unless @add_prefix.empty?
     end
 
     def emit(tag, es, chain)
-      if @add_to == 'tag_suffix'
+      if @remove_prefix and
+          ( (tag.start_with?(@removed_prefix_string) and tag.length > @removed_length) or tag == @remove_prefix)
+        tag = tag[@removed_length..-1]
+      end
+      if tag.length > 0
+        tag = @added_prefix_string + tag if @added_prefix_string
+      else
+        tag = @add_prefix
+      end
+      if @add_to == 'tag'
         tag = [tag, @hostid].join('.')
-      elsif @add_to == 'tag_prefix'
-        tag = [@hostid, tag].join('.')
       end
 
       es.each do |time, record|
